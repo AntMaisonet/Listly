8bb7e4cac56702989876d05e80a2b9ef57bf9190
Antonio M <ant.maisonet@gmail.com>
2025-03-27 20:59:33 -0400

    Remove unnecessary code

--- web.py
+++ web.py
@@ -16,15 +16,10 @@ def add_todo():
     funcs.post_todo_list(todolist)
 
 
-def complete_todo():
-    print("hi")
-
-
 st.title("My Todo List App")
 st.subheader("Welcome to the 'My Todo List' App!")
 st.write("This app was made with the hopes of increasing productivity.")
 st.write("Good Luck! - Antonio M")
-
 
 for index, todo in enumerate(todolist):
     checkbox = st.checkbox(todo, key=todo)
@@ -33,7 +28,6 @@ for index, todo in enumerate(todolist):
         funcs.post_todo_list(todolist)
         del st.session_state[todo]
         st.rerun()
-
 
 st.text_input(label="Enter a todo",
               placeholder="...",
