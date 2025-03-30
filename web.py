51b3f2326ff0f44c0a20766c10409077fbcaf163
Antonio M <ant.maisonet@gmail.com>
2025-03-30 19:24:00 -0400

    Remove unnecessary code, fix duplicate error, adjust title and subheader

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 
 """
-To Do List app - Web App Version
+Listly - A to-do list app - Web App Version
 """
 
 import streamlit as st
@@ -11,18 +11,20 @@ todolist = funcs.get_todo_list()
 
 def add_todo():
     new_todo = st.session_state["todo_txb"]
-    print(new_todo)
-    todolist.append(new_todo + "\n")
-    funcs.post_todo_list(todolist)
+    new_todo = new_todo + "\n"
 
+    if new_todo in todolist:
+        st.error("⚠️ Duplicate Item - This item already exists in the list ⚠️")
+    else:
+        todolist.append(new_todo)
+        funcs.post_todo_list(todolist)
 
-st.title("My Todo List App")
-st.subheader("Welcome to the 'My Todo List' App!")
-st.write("This app was made with the hopes of increasing productivity.")
-st.write("Good Luck! - Antonio M")
+st.write("Coded with Python and Love by Antonio Maisonet")
+st.title("Listly - Your Tasks, Simplified.")
+st.subheader("A lightweight and minimalistic to-do list app that helps you stay productive without distractions")
 
 for index, todo in enumerate(todolist):
-    checkbox = st.checkbox(todo, key=todo)
+    checkbox = st.checkbox(f"{index}. {todo}", key=todo)
     if checkbox:
         todolist.pop(index)
         funcs.post_todo_list(todolist)
